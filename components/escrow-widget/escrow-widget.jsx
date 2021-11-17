1a3b2680bd1b559b828df3b8be75abc47352d281
Fernando Lopez <lopezfer@globalhitss.com>
2021-11-17 12:53:29 -0300

    primer commit

---
+++ components/escrow-widget/escrow-widget.jsx
@@ -0,0 +1,105 @@
+import { useEffect, useRef, useState } from 'react'
+import { Box, Flex, Button, Text, Link, Heading } from '@chakra-ui/react'
+import Web3 from 'web3'
+import KlerosEscrow from '../../utils/kleros-escrow'
+
+const defaultWeb3 = new Web3(process.env.NEXT_PUBLIC_INFURA_ENDPOINT)
+
+export default function EscrowWidget({
+  web3,
+  court,
+  currency,
+  metaEvidence,
+  children,
+  amount,
+  ticker = 'ETH',
+  recipient,
+  timeout,
+  onOpen,
+  onClose,
+}) {
+  if (!web3) web3 = defaultWeb3
+
+  const klerosEscrowRef = useRef(new KlerosEscrow(web3))
+  console.log('fernando current >>>> ', klerosEscrowRef)
+  useEffect(() => {
+    klerosEscrowRef.current.setCourtAndCurrency(court, currency)
+  }, [court, currency])
+
+  const [metaEvidenceFileURI, setMetaEvidenceFileURI] = useState(
+    metaEvidence.fileURI,
+  )
+  useEffect(() => {
+    let cancelled = false
+    ;(async () => {
+      if (metaEvidence.file) {
+        const _metaEvidenceFileURI = `${
+          klerosEscrowRef.current.archon.arbitrable.ipfsGateway
+        }${await klerosEscrowRef.current.upload(
+          'metaEvidenceFile',
+          metaEvidence.file,
+        )}`
+        if (!cancelled) setMetaEvidenceFileURI(_metaEvidenceFileURI)
+      } else if (metaEvidence.fileURI !== metaEvidenceFileURI)
+        setMetaEvidenceFileURI(metaEvidence.fileURI)
+    })()
+    return () => (cancelled = true)
+  }, [metaEvidence, metaEvidenceFileURI])
+
+  const loadingRef = useRef(false)
+  const [loading, setLoading] = useState(false)
+  return (
+    <Box direction="row" align="center">
+      <Heading textAlign="center" pb={6}>
+        {metaEvidence.title}
+      </Heading>
+      <Heading fontWeight="400" size="md" textAlign="center" pb={4}>
+        {metaEvidence.description}
+      </Heading>
+      <Text>
+        Precio: {web3.utils.fromWei(String(amount))} {ticker}
+      </Text>
+      <Link newTab href={metaEvidenceFileURI}>
+        Contract
+      </Link>
+      <Flex sx={{ justifyContent: 'space-between' }} pt={10}>
+        <Button
+          // variant="secondary"
+          // sx={{ width: 80 }}
+          onClick={() => close()}
+        >
+          Regresar
+        </Button>
+        <Link
+          sx={{ alignItems: 'center', display: 'flex' }}
+          newTab
+          href="https://kleros.io"
+        >
+          <p>Por Yubiai y Kleros</p>
+        </Link>
+        <Button
+          colorScheme="blue"
+          loading={loading}
+          onClick={() => {
+            loadingRef.current = true
+            setLoading(true)
+            klerosEscrowRef.current
+              .createTransaction(amount, recipient, timeout, metaEvidence)
+              .then(() => {
+                close()
+                loadingRef.current = false
+                setLoading(false)
+              })
+              .catch(() => {
+                loadingRef.current = false
+                setLoading(false)
+                close()
+              })
+          }}
+        >
+          Pagar
+        </Button>
+      </Flex>
+    </Box>
+  )
+}
---
+++ pages/_app.js
@@ -0,0 +1,71 @@
+import dynamic from 'next/dynamic'
+import { ChakraProvider, Container, Stack } from '@chakra-ui/react'
+
+import EscrowWidget from '../components/escrow-widget/escrow-widget'
+import Web3Provider from '../utils/web3-provider'
+import ProofOfHumanity from '../subgraph/abis/proof-of-humanity.json'
+import KlerosLiquid from '../subgraph/abis/kleros-liquid.json'
+import TransactionBatcher from '../subgraph/abis/transaction-batcher.json'
+import UBI from '../subgraph/abis/ubi.json'
+import {
+  UBIAddress,
+  address,
+  klerosLiquidAddress,
+  transactionBatcherAddress,
+} from '../subgraph/config'
+
+const network = process.env.NEXT_PUBLIC_NETWORK || 'mainnet'
+
+const contracts = [
+  {
+    name: 'proofOfHumanity',
+    abi: ProofOfHumanity,
+    address: { [network]: address },
+  },
+  {
+    name: 'klerosLiquid',
+    abi: KlerosLiquid,
+    address: { [network]: klerosLiquidAddress },
+  },
+  { name: 'UBI', abi: UBI, address: { [network]: UBIAddress } },
+  {
+    name: 'transactionBatcher',
+    abi: TransactionBatcher,
+    address: { [network]: transactionBatcherAddress },
+  },
+]
+
+function App({ Component, pageProps }) {
+  const NoSSR = dynamic(
+    () => {
+      return import('../utils/web3-provider')
+    },
+    { ssr: false },
+  )
+  return (
+    <ChakraProvider>
+      <NoSSR
+        infuraURL={process.env.NEXT_PUBLIC_INFURA_ENDPOINT}
+        contracts={contracts}
+      >
+        <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
+          <Container maxW={'8xl'} pt={8} pb={4}>
+            <EscrowWidget
+              metaEvidence={{
+                title: 'Pago con Escrow',
+                description:
+                  'Camera Nikon 83x optical zoom lens, model x32a 9, additional lens.',
+                fileURI: { contract: 'someContractText' },
+              }}
+              amount={1e16}
+              recipient="0x4b93A94ca58594FAF5f64948A26F3E195Eb63B6E"
+              timeout={604800}
+            />
+          </Container>
+        </Stack>
+      </NoSSR>
+    </ChakraProvider>
+  )
+}
+
+export default App
